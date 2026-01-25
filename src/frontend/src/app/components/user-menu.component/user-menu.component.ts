4fe25384415762eb683e380bd5b74ed62854d455
Carmen Varela <cvi0004@alu.medac.es>
2026-01-25 21:32:52 +0100

    refactor css and html components and backend error correction

--- src/frontend/src/app/components/user-menu.component/user-menu.component.ts
+++ src/frontend/src/app/components/user-menu.component/user-menu.component.ts
@@ -16,7 +16,7 @@ export class UserMenuComponent {
 
   @Output() closeMenu = new EventEmitter<void>();
 
-  // Variable para controlar la visibilidad del Modal
+  // Variable to control the visibility of the Modal
   showLogoutModal = false;
   showGoodbyeModal = false;
 
@@ -25,28 +25,21 @@ export class UserMenuComponent {
     this.closeMenu.emit();
   }
 
-  // PASO 1: El usuario hace clic en "Cerrar sesión" en el menú
   onLogoutClick() {
     this.showLogoutModal = true;
   }
 
-  // PASO 2: El usuario confirma en el modal
   confirmLogout() {
-    // 1. Ocultamos el modal de pregunta
     this.showLogoutModal = false;
-
-    // 2. Mostramos el modal de despedida
     this.showGoodbyeModal = true;
 
-    // 3. Esperamos 2 segundos (2000ms) para que el usuario lo lea y sienta la calidez
     setTimeout(() => {
-      this.authService.logout(); // Logout real (borra datos y redirige)
-      this.showGoodbyeModal = false; // Limpiamos
-      this.closeMenu.emit(); // Cerramos el menú
+      this.authService.logout();
+      this.showGoodbyeModal = false;
+      this.closeMenu.emit();
     }, 2000);
   }
 
-  // PASO 3: El usuario cancela
   cancelLogout() {
     this.showLogoutModal = false;
     this.closeMenu.emit();
