0bcf18a59eebaa0d96ddd9be2c98ab9fa6e8e7ec
Carmen Varela <cvi0004@alu.medac.es>
2026-01-28 19:35:00 +0100

    footerInterface done - refactor

--- src/frontend/src/app/app.config.ts
+++ src/frontend/src/app/app.config.ts
@@ -1,17 +1,21 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth-interceptor';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  // providers list we need to add the HTTP client with the interceptor
   providers: [
-    provideRouter(routes),
-    // Tell Angular that we need de HTTP protocol with our interceptor.ts
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'enabled'
+      })
+    ),
+
     provideHttpClient(
-      // Command to Angular: “Every time you use the HTTP client, make sure it goes through this interceptor.”
       withInterceptors([authInterceptor])
     ),
     provideAnimations()
