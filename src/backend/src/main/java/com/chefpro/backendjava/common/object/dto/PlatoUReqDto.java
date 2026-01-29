516e6bf910dfff2448d0c724b2a748e89e6a358b
Maria Reyes Artacho Carrero <mariareyesartcar@gmail.com>
2026-01-29 19:56:42 +0100

    Work in progress CRUD platos y menus

---
+++ src/backend/src/main/java/com/chefpro/backendjava/common/object/dto/PlatoUReqDto.java
@@ -0,0 +1,22 @@
+package com.chefpro.backendjava.common.object.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PlatoUReqDto {
+
+  private Long id;
+  private String name;
+  private String description;
+  private String creator;
+  private Boolean vegan;
+  private Boolean vegetarian;
+  private Boolean allergies;
+  private List<IngredientsDto> ingredients;
+}
