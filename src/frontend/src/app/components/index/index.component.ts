304bbab8038a68cdb1d2c1d3e1f596be737ba1a0
Carmen Varela <cvi0004@alu.medac.es>
2026-02-06 21:39:43 +0100

    index interface

--- src/frontend/src/app/components/index/index.component.ts
+++ src/frontend/src/app/components/index/index.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
-  selector: 'app-index.component',
-  imports: [],
+  selector: 'app-index',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './index.component.html',
-  styleUrl: './index.component.css',
+  styleUrls: ['./index.component.css']
 })
 export class IndexComponent {
+  public authService = inject(AuthService);
+  private router = inject(Router);
 
+  navigateTo(path: 'CHEF' | 'DINER') {
+    const user = this.authService.currentUserValue;
+
+    if (!user) {
+      this.router.navigate(['/signup'], { queryParams: { role: path } });
+    } else {
+      this.router.navigate(['/profile']);
+    }
+  }
 }
