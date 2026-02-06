e0e7ba395b04ffaccefce7362757b7d0ee0507f1
Carmen Varela Iglesias <cvi0004@alu.medac.es>
2026-02-06 11:22:25 +0100

    Add local development environment with Docker Compose

---
+++ src/frontend/src/app/components/create-menu/create-menu.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-menu',
+  standalone: true,
+  template: '<p>create-menu component works!</p>',
+  styles: [],
+})
+export class CreateMenuComponent {}
