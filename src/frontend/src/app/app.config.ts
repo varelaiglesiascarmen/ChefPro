1484af8fb227377736948f96b19e8db90a59b05a
Carmen Varela <carvarigle@gmail.com>
2026-02-23 23:13:44 +0100

    “About us” interface completed (#85)
    @reyes-art-car needs to upload his photo to src/frontend/public/team
    with the name ‘reyes-team.png’ so that his photo can be displayed in his
    project biography.

--- src/frontend/src/app/app.config.ts
+++ src/frontend/src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideRouter, withInMemoryScrolling, withRouterConfig } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth-interceptor';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,12 +12,10 @@ export const appConfig: ApplicationConfig = {
       withInMemoryScrolling({
         anchorScrolling: 'enabled',
         scrollPositionRestoration: 'enabled'
-      })
+      }),
+      withRouterConfig({ onSameUrlNavigation: 'reload' })
     ),
-
-    provideHttpClient(
-      withInterceptors([authInterceptor])
-    ),
-    provideAnimations(),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideAnimations() 
   ]
 };
