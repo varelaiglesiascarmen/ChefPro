f601c33d1367090fc2c5f799704ba3519363c784
Carmen Varela <carvarigle@gmail.com>
2026-05-16 20:20:46 +0200

    enlace arreglado

--- src/frontend/src/app/components/home-page/home-page.component.ts
+++ src/frontend/src/app/components/home-page/home-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../models/auth.model';
 
 @Component({
   selector: 'app-home-page',
@@ -9,8 +11,17 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  private authService = inject(AuthService);
   isMenuOpen = false;
+
+  currentUser: User | null = null;
+
+  ngOnInit() {
+    this.authService.user$.subscribe(user => {
+      this.currentUser = user;
+    });
+  }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
