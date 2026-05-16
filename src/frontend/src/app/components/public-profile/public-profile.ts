d3ab892d09d850bd8bd55d0fb23a5bb3fe34b539
Carmen Varela <cvi0004@alu.medac.es>
2026-05-16 21:39:34 +0200

    component creation

---
+++ src/frontend/src/app/components/public-profile/public-profile.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-public-profile',
+  imports: [],
+  templateUrl: './public-profile.html',
+  styleUrl: './public-profile.css',
+})
+export class PublicProfile {
+
+}
