3ae11218b33d93c8aa469b1c946aa4426c6c544f
Carmen Varela <carvarigle@gmail.com>
2026-05-17 17:36:52 +0200

    ok

--- src/backend/src/main/java/com/chefpro/backendjava/common/object/dto/MenuCReqDto.java
+++ src/backend/src/main/java/com/chefpro/backendjava/common/object/dto/MenuCReqDto.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.constraints.DecimalMin;
@@ -29,13 +30,26 @@ public class MenuCReqDto {
   @NotNull(message = "pricePerPerson is required")
   @DecimalMin(value = "0.01", message = "pricePerPerson must be greater than 0")
   @JsonProperty("price_per_person")
+  @JsonAlias({ "pricePerPerson" })
   private BigDecimal pricePerPerson;
 
+  @JsonProperty("min_number_diners")
+  @JsonAlias({ "minNumberDiners" })
   private Integer minNumberDiners;
+
+  @JsonProperty("max_number_diners")
+  @JsonAlias({ "maxNumberDiners" })
   private Integer maxNumberDiners;
+
+  @JsonProperty("kitchen_requirements")
+  @JsonAlias({ "kitchenRequirements" })
   private String kitchenRequirements;
 
+  @JsonProperty("chef_ID")
+  @JsonAlias({ "chef_id", "chefId" })
   private Long chefId;
 
+  @JsonProperty("dish_ids")
+  @JsonAlias({ "dishIds" })
   private List<Long> dishIds;
 }
